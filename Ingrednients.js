24dfe789be02d64de028afc643d134a72e7048c6
Josh194812 <69601082+Josh194812@users.noreply.github.com>
2020-09-22 20:09:45 -0500

    Add files via upload

---
+++ Ingrednients.js
@@ -0,0 +1,6 @@
+/*large hard-boiled EGGS, sliced	6
+mayonnaise	1/4 cup
+fresh lemon juice	2 tsp.
+minced onion	1 Tbsp.
+finely chopped celery	1/2 cup
+Lettuce leaves (for serving)*/	
---
+++ instructions.js
@@ -0,0 +1,7 @@
+/*RESERVE and refrigerate 4 center egg slices for garnish, if desired. CHOP remaining eggs.
+
+MIX mayonnaise, lemon juice, onion, salt and pepper in medium bowl. ADD chopped eggs and celery; MIX well. REFRIGERATE, covered, to blend flavors.
+
+SERVE on lettuce leaves, garnished with reserved egg slices.
+
+Easy 12-Minute Method for Hard-Boiled Eggs: PLACE eggs in a saucepan large enough to hold them in a single layer. ADD cold water to cover the eggs by 1 inch. HEAT over high heat just to boiling. REMOVE from the burner. COVER pan. Let eggs stand in hot water for about 12 minutes for large eggs (9 minutes for medium eggs; 15 minutes for extra large eggs). DRAIN. SHOCK the eggs in a bowl of ice water to cool them immediately. Hard-boiled eggs are easiest to peel right after cooling.*/
